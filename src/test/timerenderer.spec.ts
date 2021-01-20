943df3e28ddb079304759bfed5a7d7d8e3efe583
Chethana Bellechalu <chethanax.bellechalu@intel.com>
2021-01-20 17:39:15 +0000

    fix(tests): Fix timezone issue in timerenderer unit test

--- src/test/timerenderer.spec.ts
+++ src/test/timerenderer.spec.ts
@@ -8,9 +8,12 @@ import {TimeRenderer} from '../reactjs/components/shared/TimeRenderer';
 describe('Test time renderer component', () => {
     it('should return the formatted time from the component', () => {
         const time: any = {
-            value: new Date('2020-12-18')
+            value: new Date(2021, 0, 20)
         };
+        const adjustedTime = new Date(time.value.getTime() - (time.value.getTimezoneOffset() * 60000));
+        const timeSplit = adjustedTime.toISOString().split("T");
+        const concatedTime = timeSplit[0].concat(" ").concat(timeSplit[1].substring(0, 5))
         const result = TimeRenderer(time);
-        expect(result).toEqual(`2020-12-18 00:00`)
+        expect(result).toEqual(concatedTime)
     })
 })
