393d505e48a31746e8ab741b96e857732d001ec4
Chethana Bellechalu <chethanax.bellechalu@intel.com>
2021-04-07 03:06:59 +0000

    feat(device): removed device control
    BREAKING CHANGE: device react control is no longer supported

--- dist/lib/src/reactjs/components/mps.d.ts
+++ dist/lib/src/reactjs/components/mps.d.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Intel Corporation 2020
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
-import { DeviceGrid } from './DeviceGrid/DeviceGrid';
 import { RemoteDesktop } from './KVM/UI';
 import { Sol } from './SerialOverLAN/Sol';
 import { Provider as MpsProvider } from './shared/context/BasicContextProvider';
--- src/reactjs/components/mps.tsx
+++ src/reactjs/components/mps.tsx
@@ -3,9 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
 
-import { DeviceGrid } from './DeviceGrid/DeviceGrid'
 import { RemoteDesktop } from './KVM/UI'
 import { Sol } from './SerialOverLAN/Sol'
 import { Provider as MpsProvider } from './shared/context/BasicContextProvider'
 
-export { DeviceGrid, RemoteDesktop, Sol, MpsProvider }
+export { RemoteDesktop, Sol, MpsProvider }
