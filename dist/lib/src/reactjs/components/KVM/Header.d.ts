3e1df6e104c873832d385824d42a941f0e7cb621
madhavi-losetty <madhavi.losetty@intel.com>
2021-04-21 21:09:04 -0700

    refactor(apis): removed rest apis from react components

--- dist/lib/src/reactjs/components/KVM/Header.d.ts
+++ dist/lib/src/reactjs/components/KVM/Header.d.ts
@@ -11,23 +11,6 @@ export interface IHeaderProps {
     changeDesktopSettings: (settings: any) => void;
     getConnectState: () => number;
 }
-export interface PowerStates {
-    powerState: number;
-    showSuccess: boolean;
-    message: string;
-    isSelected: boolean;
-    type: string;
-    kvmNotEnabled: string;
-    deviceOnSleep: string;
-    isPowerStateLoaded: boolean;
-}
-export declare class Header extends React.Component<IHeaderProps, PowerStates> {
-    constructor(props: IHeaderProps);
-    /** send power actions to AMT device */
-    handlePowerOptions: (e: any) => Promise<any>;
-    /** callback functions from child components to update the state values */
-    handleFeatureStatus: (value: any) => void;
-    handlePowerStatus: (value: any) => void;
-    updatePowerStatus: () => void;
+export declare class Header extends React.Component<IHeaderProps> {
     render(): JSX.Element;
 }
