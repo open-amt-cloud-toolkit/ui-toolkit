c4ad56e49028d38269cfe3a92acb9be69c8de022
Bellechalu <chethanax.bellechalu@intel.com>
2021-04-26 22:27:28 +0530

    fix(webpack): update output configuration for webpack production bundles

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -19,7 +19,9 @@ module.exports = {
 
   output: {
     filename: "[name].min.js",
-    path: path.resolve(__dirname, "./dist")
+    path: path.resolve(__dirname, "./dist"),
+    library: 'amtlibrary',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
