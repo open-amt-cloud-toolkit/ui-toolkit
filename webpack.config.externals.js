aabc4f9a49baadde18f5e8982f694de97d58aacd
Ubuntu <vinay-mps@mps-ubuntu-2.kk4kidx0ywwe1bl1wzin4httqh.tx.internal.cloudapp.net>
2021-04-30 11:00:50 +0000

    fix(webpack): updated webpack.config.external file

--- webpack.config.externals.js
+++ webpack.config.externals.js
@@ -21,9 +21,7 @@ module.exports = {
 
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    library: '',
-    libraryTarget: 'commonjs'
+    path: path.resolve(__dirname, "./dist")
   },
   externals: [nodeExternals()],
   module: {
