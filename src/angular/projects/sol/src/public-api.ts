656cc286a02088220747e981ec94329f52011247
mahendrax-reddy <mahendrax.reddy@intel.com>
2021-07-13 13:29:19 +0530

    feat(webui): angular sol component

--- src/angular/projects/sol/src/public-api.ts
+++ src/angular/projects/sol/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of sol
  */
 
-export * from './lib/sol.service';
-export * from './lib/sol.component';
-export * from './lib/sol.module';
+export * from './lib/sol.service'
+export * from './lib/sol.component'
+export * from './lib/sol.module'
